511d9ba93cbe071cba41cd3b68eea6d0e7528085
KNR1997 <kethakaranasinghe@gmail.com>
2024-01-26 15:14:31 +0530

    jwt add

--- usedProductsStore/urls.py
+++ usedProductsStore/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
