928e342495a9a76a985f1b27b6804ecdccc922c4
KNR1997 <kethakaranasinghe@gmail.com>
2024-01-29 22:43:29 +0530

    project cleanup

--- base/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books', views.getBooks),
-    path('books/<int:id>/', views.getBook),
-
-    path('reviews/search/findByBookId/<int:id>/', views.getReviewsByBookId)
-]
--- client/src/lib/oktaConfig.ts
+++
@@ -1,8 +0,0 @@
-export const oktaConfig = {
-    clientId: '0oa91q1b03bsRbwA95d7',
-    issuer: 'https://dev-79977778.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['openid', 'profile', 'email'],
-    pkce: true,
-    disableHttpsCheck: true,
-}
